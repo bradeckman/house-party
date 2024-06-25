68baeeca6e8acb051a2b99876ed70f0ff98bfe3e
Brad Eckman <brad@dotcompliance.com>
2024-06-25 19:32:08 +0300

    after 16

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--6traqh_l6w6b2lasa+k4raxra*&q3$+gh@5e8@+!3%$a7=31_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
